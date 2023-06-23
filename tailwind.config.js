e0ffb2c17c057aa50ea997e75b2978c83535fd9f
Ruben Reyes <rubenreyesdaniel@gmail.com>
2023-06-23 11:54:56 -0600

    se agrego tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{vue}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "media"
+}
+
